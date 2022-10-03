a7214338946790d06187c5ce0fed5100578c6a39
luckman212 <1992842+luckman212@users.noreply.github.com>
2022-10-03 16:29:18 -0400

    upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# wgfix
+# wgfix v2.0
+
+# Installation instructions
+
+1. Apply commit XXX using System Patches
+2. Add `@` to the end of the description of any **peers** that you want to fail back after WANUP events, e.g. rename `my_peer` → `my_peer@`
+
+# Testing
+
+You can manually test the script via console/ssh:
+
+```shell
+/root/wgfix.sh <GW_NAME> 0
+```
--- wgfix.sh
+++ wgfix.sh
@@ -1,71 +1,95 @@
 #!/bin/sh
 
-# put the line below at the end of /etc/rc.gatway_alarm, just above the final `exit`:
-# /root/wgfix.sh "${GW}" "${alarm_flag}"
+# wgfix v2.0
+# https://github.com/luckman212/wgfix
 
-# set the 2 variables below to match the interface name and public key
-# of the wg tunnel that you want to "fail back" when your default gateway changes
-# WG_PEER_PUBLIC_KEY should be the public key from the FAR side (i.e the one from the PEERS tab)
-WG_IFNAME='tun_wg1'
-WG_PEER_PUBLIC_KEY='WxYy9KQuut6ZTjVze+H/cH4Es3dc0ATn8RQgw6VDiQA='
+# install instructions:
+# =============================================================================
+# 1. apply commit from the README using System Patches
+# 2. add `@` to the end of the description of any peers that you want to fail
+#    back after WANUP events, e.g. 'my_peer@'
+#
+# testing
+# =============================================================================
+# you can manually test the script via console/ssh:
+# /root/wgfix.sh <GW_NAME> 0
 
-acquire_lock() {
+_log() {
+  /usr/bin/logger -t wgfix "$1"
+  echo "$1"
+}
+
+_acquire_lock() {
   if /bin/pgrep -F "$LOCKFILE" >/dev/null 2>&1; then
-    /usr/bin/logger -t wgfix "lockfile present, aborting"
+    _log "lockfile present, aborting"
     exit 1
   fi
-  /usr/bin/logger -t wgfix "acquiring lockfile"
+  _log "acquiring lockfile"
   echo $$ >"$LOCKFILE"
 }
 
-die() {
-  /usr/bin/logger -t wgfix "done, removing lockfile"
+_die() {
+  _log "done, removing lockfile"
   [ -f "$LOCKFILE" ] && rm "$LOCKFILE"
   exit $1
 }
 
-LOCKFILE="/tmp/${0##*/}.lock"
-/usr/bin/logger -t wgfix "$0 called, args: $1 $2"
-# the point of this script is "fail back" so we only care about "WAN up" events
-if [ "$2" != "0" ]; then
-  /usr/bin/logger -t wgfix "ignoring WAN down event"
-  die 0
-fi
-acquire_lock
-/usr/bin/logger -t wgfix "WAN UP: $1"
-
-/usr/local/bin/wg showconf $WG_IFNAME |
-/usr/bin/awk -v PK="$WG_PEER_PUBLIC_KEY" '
-  BEGIN {FS=" = "}
-  ($1 == "PublicKey" && $2 == PK) {f=1}
-  /^Endpoint/ && f {e=$2}
-  /^$/ {f=""}
-  END {if(e) {print e}}' >/tmp/${WG_IFNAME}_endpoint
-
-IFS=: read -r IP PORT </tmp/${WG_IFNAME}_endpoint
-if [ -n "$IP" ] && [ -n "$PORT" ]; then
-  /usr/bin/logger -t wgfix "WG endpoint: $IP:$PORT"
-  /usr/bin/logger -t wgfix "pausing 10s to allow gateway change to occur"
-  /bin/sleep 10
-  DEF_GW=$(/sbin/route -n get "$IP" | /usr/bin/awk '/interface:/ {print $2; exit;}')
-  /usr/bin/logger -t wgfix "Default gateway iface: $DEF_GW"
-  BAD_STATES=$(/sbin/pfctl -vvss | /usr/bin/grep "$IP:$PORT" | /usr/bin/grep -v "$DEF_GW" | wc -l)
-  if [ "$BAD_STATES" -gt 0 ]; then
-    /usr/bin/logger -t wgfix "found $BAD_STATES bad states; bouncing wg service"
-    /usr/local/bin/php_wg -f /usr/local/pkg/wireguard/includes/wg_service.inc stop
-    /sbin/pfctl -vvss |
-    /usr/bin/grep -A2 "$IP:$PORT" |
-    /usr/bin/awk 'BEGIN {OFS="/"} /id:/ {print $2,$4}' |
-    while read -r STATE; do
-      /usr/bin/logger -t wgfix "killing state $STATE"
-      /sbin/pfctl -k id -k "$STATE"
-    done
-    /usr/local/bin/php_wg -f /usr/local/pkg/wireguard/includes/wg_service.inc start
+_failback() {
+  IP=$1
+  PORT=$2
+  if [ -n "$IP" ] && [ -n "$PORT" ]; then
+    _log "WG endpoint: $IP:$PORT"
+    _log "pausing 10s to allow gateway change to occur"
+    /bin/sleep 10
+    DEF_GW=$(/sbin/route -n get "$IP" | /usr/bin/awk '/interface:/ {print $2; exit;}')
+    _log "Default gateway iface: $DEF_GW"
+    BAD_STATES=$(/sbin/pfctl -vvss | /usr/bin/grep "$IP:$PORT" | /usr/bin/grep -v "$DEF_GW" | wc -l)
+    if [ "$BAD_STATES" -gt 0 ]; then
+      _log "found $BAD_STATES bad states; bouncing wg service"
+      /usr/local/bin/php_wg -f /usr/local/pkg/wireguard/includes/wg_service.inc stop
+      /sbin/pfctl -vvss |
+      /usr/bin/grep -A2 "$IP:$PORT" |
+      /usr/bin/awk 'BEGIN {OFS="/"} /id:/ {print $2,$4}' |
+      while read -r STATE; do
+        _log "killing state $STATE"
+        /sbin/pfctl -k id -k "$STATE"
+      done
+      /usr/local/bin/php_wg -f /usr/local/pkg/wireguard/includes/wg_service.inc start
+    else
+      _log "no bad states found"
+    fi
   else
-    /usr/bin/logger -t wgfix "no bad states found"
+    _log "WG endpoint could not be determined"
   fi
-else
-  /usr/bin/logger -t wgfix "WG endpoint could not be determined"
-fi
+}
 
-die 0
+LOCKFILE="/tmp/${0##*/}.lock"
+_log "$0 called, args: $1 $2"
+if [ -z "$2" ] || [ "$2" -ne 0 ]; then
+  # the point of this script is to "fail back" so we only care about "WAN UP" events
+  _log "ignoring signal ($2) since it was not a WAN UP event"
+  _die 0
+fi
+_acquire_lock
+_log "WAN UP: $1"
+
+# iterate and find peers / endpoints to fail trigger failback
+# TODO: when php8 is available, change to str_ends_with()
+# https://www.php.net/manual/en/function.str-ends-with.php
+/usr/local/bin/php -r '
+require_once("wireguard/includes/wg.inc");
+if (is_array($wgg["peers"]) && count($wgg["peers"]) > 0) {
+  $x = array_filter($wgg["peers"], function($v, $k) {
+    return substr($v["descr"], -1) == "@";
+  }, ARRAY_FILTER_USE_BOTH);
+  foreach ($x as $peer_idx => $peer) {
+    echo $peer["descr"] . "|" . $peer["tun"] . "|" . $peer["endpoint"] . "|" . $peer["port"] . "\n";
+  }
+}
+' |
+while IFS='|' read -r DESC IFNAME ENDPOINT PORT; do
+  _log "checking $DESC ($IFNAME) at $ENDPOINT:$PORT"
+  _failback $ENDPOINT $PORT
+done
+
+_die 0
